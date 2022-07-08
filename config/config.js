2a046281b8e7e17ab3bd78305cc1b457c4b46927
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-07-08 20:42:40 +0300

    fix mongo uri

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: 'mongodb+srv://kasiliwachiye:bOn2jiRwLfofMWuZ@cluster0.zoudst1.mongodb.net/?retryWrites=true&w=majority'
+  mongoUri: 'mongodb+srv://kasiliwachiye:bOn2jiRwLfofMWuZ@cluster0.zoudst1.mongodb.net/?retryWrites=true&w=majority' || 'mongodb://localhost:27017/expense-tracker',
 }
 
 export default config
