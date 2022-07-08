b595d6b1f8cf4acfcd6e2b67ca1021da6a848ff6
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-07-08 20:31:05 +0300

    fixed port

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: 'mongodb+srv://kasiliwachiye:bOn2jiRwLfofMWuZ@cluster0.zoudst1.mongodb.net/?retryWrites=true&w=majority'
 }
