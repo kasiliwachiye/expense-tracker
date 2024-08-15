5a489ceec955fd65cd3f3ffa8cccbd943a7391f1
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-08-15 11:50:00 +0300

    chore: update config file

--- config/config.js
+++ config/config.js
@@ -1,8 +1,9 @@
 const config = {
-  env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT,
-  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: 'mongodb+srv://kasiliwachiye:bOn2jiRwLfofMWuZ@cluster0.zoudst1.mongodb.net/?retryWrites=true&w=majority' || 'mongodb://localhost:27017/expense-tracker',
-}
+  env: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "pochi",
+  mongoUri:
+    process.env.MONGO_URI || "mongodb://localhost:27017/expense-tracker",
+};
 
-export default config
+export default config;
