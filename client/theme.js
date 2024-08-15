bae7c67eea62fe2cf170e0333b3c43823f315e20
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-08-15 11:40:59 +0300

    fix: update createMUITheme to createTheme

--- client/theme.js
+++ client/theme.js
@@ -1,24 +1,24 @@
-import { createMuiTheme } from '@material-ui/core/styles'
-import { cyan, blue } from '@material-ui/core/colors'
+import { createTheme } from "@material-ui/core/styles";
+import { cyan, blue } from "@material-ui/core/colors";
 
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     primary: {
-    light: '#DBE2EF',
-    main: '#3F72AF',
-    dark: '#364F6B',
-    contrastText: '#fff',
+      light: "#DBE2EF",
+      main: "#3F72AF",
+      dark: "#364F6B",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#EFBBCF",
+      main: "#C3AED6",
+      dark: "#8675A9",
+      contrastText: "#000",
+    },
+    openTitle: blue["700"],
+    protectedTitle: cyan["700"],
+    type: "light",
   },
-  secondary: {
-    light: '#EFBBCF',
-    main: '#C3AED6',
-    dark: '#8675A9',
-    contrastText: '#000',
-  },
-    openTitle: blue['700'],
-    protectedTitle: cyan['700'],
-    type: 'light'
-  }
-})
+});
 
-export default theme  
+export default theme;
