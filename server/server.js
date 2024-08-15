7e1a79ac1d95c0e11dc055a7f18772fb72f53432
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-08-15 12:58:59 +0300

    chore: update the connection url

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
-import config from './../config/config'
-import app from './express'
-import mongoose from 'mongoose'
+import config from "./../config/config";
+import app from "./express";
+import mongoose from "mongoose";
 
 // Connection URL
-mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${config.mongoUri}`)
-})
+mongoose.Promise = global.Promise;
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(config.mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.info("Successfully connected to the database");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    throw new Error(`Unable to connect to database: ${config.mongoUri}`);
+  }
+};
+
+connectToDatabase();
 
 app.listen(config.port, (err) => {
   if (err) {
-    console.log(err)
+    console.error(err);
+  } else {
+    console.info("Server started on port %s.", config.port);
   }
-  console.info('Server started on port %s.', config.port)
-})
+});
